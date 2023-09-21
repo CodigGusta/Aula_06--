59bf9452f100f0a4149f5b63ae431c9a80abb095
CodigGusta <144278048+CodigGusta@users.noreply.github.com>
2023-09-21 14:26:45 -0300

    Exercicios da aula 06

---
+++ main.py
@@ -0,0 +1,63 @@
+# # minha_lista = [1, 2, 3, 4, 5]
+# primeiro_elemento = minha_lista[0] #variedade de dados menos digitação
+# print(primeiro_elemento)  # Isso imprimirá 1
+
+# lista = [1,3,6,5,5, "casa", 2.8, True] 
+# lista.append(2) #adiciona algo a lista
+# print(lista) 
+#---------------------------------------------------
+# lista2 = [1,11,3,6,88,12,"cc"]
+# lista2.remove(11)
+# print(lista2)
+# del lista2[0]
+# print(lista2)
+# tamanho = len(lista2) #conta os indices restantes 
+# print(tamanho)
+#----------------------------------------------
+# lista5 = [22,98,1,11,"casa"]
+# for i in lista5:
+#   print(i)
+#Exercício 1: Crie uma lista chamada `numeros` que contenha os números inteiros de 1 a 5 e imprima-a na tela.
+# numeros = [1, 2, 3, 4, 5]
+# print(numeros)
+# #Exercício 2:Acesse e imprima o terceiro elemento da lista `numeros`.
+# print(numeros[2])
+# # 3- Adicione o número 6 à lista `numeros` e imprima a lista atualizada.
+# numeros.append(6)
+# print(numeros)
+# #Exercício 4: Remova o número 3 da lista numeros e imprima a lista resultante.
+# numeros.remove(3)
+# print(numeros)
+# #Exercício 5:** Crie uma lista chamada `frutas` contendo três nomes de frutas diferentes. Em seguida, concatene essa lista com a lista `numeros` e imprima o resultado.
+# frutas = ['maça', 'uva', 'pera']
+# lista_combinada = numeros + frutas
+# print(lista_combinada)
+# #Exercício 6:** Use um loop `for` para percorrer e imprimir todos os elementos da lista `frutas`.
+# for i in frutas:
+#  print(i)
+# #Exercício 7:** Verifique se o número 5 está presente na lista `numeros` e imprima uma mensagem informando se ele está na lista ou não.
+
+# numeros = [1,3,6,7,25,16]
+
+# if 5 in numeros:
+#  print('O número está na lista')
+# else:
+#   print('O número não esta na lista')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
